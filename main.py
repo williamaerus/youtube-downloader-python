4426acf4707c7f39b7499da358276798691b6674
williamaerus <113334672+williamaerus@users.noreply.github.com>
2022-10-09 14:47:56 +0200

    implementation of notifications (LINUX)
    uncomment line 2,12,13 to get notification once the video is succesfully downloaded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 from sys import argv
-
+#from gi.repository import Notify
 link = argv[1]
 yt = YouTube(link)
 
@@ -9,6 +9,7 @@ print("Title: ", yt.title)
 print("View: ", yt.views)
 
 yd = yt.streams.get_highest_resolution()
-
+#Notify.init("Youtube downloader")
+#Notify.Notification.new(yt.title," succesfully downloaded").show()
 # ADD FOLDER HERE
-yd.download('./')
+yd.download('/home/william/Downloads/youtube-download')
